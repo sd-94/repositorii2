b80ef00d0c02ae0f5206dad76c13e177cbdbc790
Artyom Dragosh <sd9265479529@gmail.com>
2024-03-13 23:56:00 +0300

    Commit 3

---
+++ file3.go
@@ -0,0 +1,7 @@
+package main
+
+import "fmt"
+
+func file3() {
+    fmt.Println("This text was changed in new-branch-1.")
+}
--- main.go
+++ main.go
@@ -5,4 +5,5 @@ import "fmt"
 func main() {
 	fmt.Println("This text was changed in new-branch-1.")
 	file2()
+	file3()
 }
