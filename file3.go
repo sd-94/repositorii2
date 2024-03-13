025dd10b4918f7a8e9146a097d124a35a27ceb30
Artyom Dragosh <sd9265479529@gmail.com>
2024-03-14 00:01:26 +0300

    Revert "Commit 3"
    This reverts commit b80ef00d0c02ae0f5206dad76c13e177cbdbc790.

--- file3.go
+++
@@ -1,7 +0,0 @@
-package main
-
-import "fmt"
-
-func file3() {
-    fmt.Println("This text was changed in new-branch-1.")
-}
--- main.go
+++ main.go
@@ -5,5 +5,4 @@ import "fmt"
 func main() {
 	fmt.Println("This text was changed in new-branch-1.")
 	file2()
-	file3()
 }
